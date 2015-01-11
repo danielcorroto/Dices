e554b76a30f73bbb75068dcfe2716a64498d30cc
Daniel Corroto <danicq85@gmail.com>
2015-01-12 00:31:27 +0100

    Interface modification

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-const values = [4,6,8,10,10,12,20,100];
-const ids = ["qtyd4", "qtyd6", "qtyd8", "qtyd10", "qtyd00", "qtyd12", "qtyd20", "qtyd100"];
+var selected = 6;
+var quantity = 0;
 
-var dices = [0,0,0,0,0,0,0,0]; // 4, 6, 8, 10, 00, 12, 20, 100
-
-function printDiceQty(index) {
-	var span = $("#"+ids[index]);
-	span.text(dices[index]);
-	if (dices[index] == 0) {
+function printDiceQty() {
+	var span = $("#quantity");
+	span.text(quantity);
+	if (quantity == 0) {
 		span.removeClass("label-success");
 		span.addClass("label-default");
 	} else {
@@ -15,49 +13,30 @@ function printDiceQty(index) {
 	}
 }
 
-function clearDices() {
-	for (var i=0; i<dices.length; i++) {
-		dices[i] = 0;
-		printDiceQty(i);
+function addDice(value) {
+	quantity += value;
+	if (value < 0) {
+		value = 0;
 	}
+	printDiceQty();
 }
 
-function addDice(add, dice) {
-	var value;
-	if (add) {value = 1;}
-	else {value = -1;}
-	
-	var index = -1;
-	if (dice == "d4") { index = 0; }
-	else if (dice == "d6") { index = 1;}
-	else if (dice == "d8") { index = 2;}
-	else if (dice == "d10") { index = 3;}
-	else if (dice == "d00") { index = 4;}
-	else if (dice == "d12") { index = 5;}
-	else if (dice == "d20") { index = 6;}
-	else if (dice == "d100") { index = 7;}
-	
-	dices[index] += value;
-	if (dices[index] < 0) {
-		dices[index] = 0;
-	}
-	printDiceQty(index);
+function clearDices() {
+	quantity = 0;
+	printDiceQty();
 }
 
 function rollDices() {
 	var res = [];
 	var total = 0;
-	for (var i=0; i<dices.length; i++) {
-		for (var j=0; j<dices[i]; j++) {
-			var value = Math.floor(Math.random() * values[i]);
-			if (i==4) { value *= 10; }
-			else { value += 1; }
-			res.push(value);
-			total += value;
-		}
+	for (var i=0; i<quantity; i++) {
+		var value = Math.floor(Math.random() * selected);
+		if (selected != 100) { value += 1; }
+		res.push(value);
+		total += value;
 	}
 	if (res.length > 0) {
-		$("#result").text("Roll " + res);
+		$("#result").text("Roll: " + res);
 		$("#total").text("Total: " + total);
 	}
 }
